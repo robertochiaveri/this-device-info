e633fcaf666960d32d50f12a7e166bde9d65cc26
Roberto Chiaveri <robertochiaveri@users.noreply.github.com>
2020-01-30 23:29:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+
+window.thisDeviceInfo = require("./thisDeviceInfo");
+
+/*
+
+  USAGE:
+  ======
+
+  1. for each module needed, repeat:
+  ----------------------------------
+    thisDeviceInfo.loadModule(
+      moduleName [string],
+      module [function],
+      (optional) listenersList [array]
+    );
+
+
+  2. finally:
+  -----------
+    thisDeviceInfo.init({
+      throttleInterval: [number] milliseconds
+      callbackFn: [function] a callback called at every update cycle to display output
+    });
+
+
+  3. optionally:
+  --------------
+    add #debug to the URL to display raw JSON output
+
+*/
+
+thisDeviceInfo.loadModule("navigatorInfo", require('./modules/navigatorInfo'));
+
+thisDeviceInfo.loadModule("screenInfo", require('./modules/screenInfo'));
+
+thisDeviceInfo.loadModule("gyroscopeInfo", require('./modules/gyroscopeInfo'));
+
+thisDeviceInfo.loadModule("motionSensorsInfo", require('./modules/motionSensorsInfo'));
+
+thisDeviceInfo.loadModule("webGLInfo", require('./modules/webGLInfo'));
+
+thisDeviceInfo.loadModule("phonegapDeviceInfo", require('./modules/phonegapDeviceInfo'));
+
+thisDeviceInfo.loadModule("batteryInfo", require('./modules/batteryInfo'));
+
+thisDeviceInfo.loadModule("connectionInfo", require('./modules/connectionInfo'));
+
+thisDeviceInfo.loadModule("mediaCaptureInfo", require('./modules/mediaCaptureInfo'));
+
+thisDeviceInfo.loadModule("lightInfo", require('./modules/lightInfo'));
+
+thisDeviceInfo.loadModule("userAgentInfo", require('./modules/userAgentInfo'));
+
+thisDeviceInfo.loadModule("UALookupInfo", require('./modules/uaLookupInfo'));
+
+thisDeviceInfo.loadModule("IPLookupInfo", require('./modules/ipLookupInfo'));
+
+thisDeviceInfo.init({
+  callbackFn: require("./template/ubiss/js/output")
+});
