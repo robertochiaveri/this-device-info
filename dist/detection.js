de5a1e8231b566483e64aaf49f5c982a0ac258e9
Roberto Chiaveri <robertochiaveri@users.noreply.github.com>
2020-02-10 22:29:24 +0100

    new implementation of ambient light info

--- dist/detection.js
+++ dist/detection.js
@@ -423,17 +423,16 @@ module.exports = (function(){
 
       // ambient light sensor --- almost deprecated in 2020
       if ("AmbientLightSensor" in window) {
-        try{
+        try {
 
           var lightSensor = new AmbientLightSensor({frequency: 1}),
               ambientLightInfoEvent,
               detail = {};
 
-          console.log("Starting AmbientLightSensor...",lightSensor);
+          lightSensor.addEventListener('reading', function(event) {
 
-          lightSensor.onReading = function(event) {
-
-            console.log("--- AmbientLightInfoEvent ---",e);
+            console.log("--- AmbientLightInfoEvent ---",event);
+            alert("lux")
 
             if (event.target) {
 
@@ -453,10 +452,9 @@ module.exports = (function(){
 
             }
 
-            if (event.target && event.target.illuminance) {
-              detail.illuminance = event.target.illuminance + " lux";
-            }
-
+            lightSensor.start();
+            console.log("Starting AmbientLight sensor",lightSensor);
+  
             ambientLightInfoEvent = new CustomEvent("__AmbientLightInfoEvent", {
               detail: detail,
               bubbles: true,
@@ -464,70 +462,73 @@ module.exports = (function(){
             });
             dispatchEvent(ambientLightInfoEvent);
 
-          };
-          lightSensor.start();
+          });
+
+
 
         } catch(e) {
           console.log("ERROR: can't access AmbientLightSensor",e)
         }
+      } else {
+        console.log("ERROR: AmbientLightSensor is not supported")        
       }
 
       
       var init = function(event) {
 
-        var ambientLight = {};
+        // var ambientLight = {};
 
-        if (typeof window.matchMedia == "function") {
+        // if (typeof window.matchMedia == "function") {
           
-          if (window.matchMedia("(luminosity: dim)").matches) {
-            ambientLight.luminosity = "low";
-          }
+        //   if (window.matchMedia("(luminosity: dim)").matches) {
+        //     ambientLight.luminosity = "low";
+        //   }
 
-          if (window.matchMedia("(luminosity: normal)").matches) {
-            ambientLight.luminosity = "normal";
-          }
+        //   if (window.matchMedia("(luminosity: normal)").matches) {
+        //     ambientLight.luminosity = "normal";
+        //   }
 
-          if (window.matchMedia("(luminosity: washed)").matches) {
-            ambientLight.luminosity = "high";
-          }
+        //   if (window.matchMedia("(luminosity: washed)").matches) {
+        //     ambientLight.luminosity = "high";
+        //   }
 
-        }
+        // }
 
-        if (event && event.type) {
+        // if (event && event.type) {
 
-          switch (event.type) {
+        //   switch (event.type) {
 
-            case "devicelight":
-              if (event.value) {
-                ambientLight["Intensity"] = event.value +" lux";
-              }
-              break;
+        //     case "devicelight":
+        //       if (event.value) {
+        //         ambientLight["Intensity"] = event.value +" lux";
+        //       }
+        //       break;
 
-            case "lightlevel":
-              if (event.lux) {
-                ambientLight["Level"] = event.value;
-              }
-              break;
+        //     case "lightlevel":
+        //       if (event.lux) {
+        //         ambientLight["Level"] = event.value;
+        //       }
+        //       break;
 
-            case "__AmbientLightInfoEvent":
-              if (event.detail) {
+        //     case "__AmbientLightInfoEvent":
+        //       if (event.detail) {
 
-                if (event.detail.illuminance) {
-                  ambientLight["Illuminance"] = event.detail.illuminance;
-                }
-                if (event.detail.state) {
-                  ambientLight["Sensor"] = event.detail.state;
-                }
-              }
-            break;
+        //         if (event.detail.illuminance) {
+        //           ambientLight["Illuminance"] = event.detail.illuminance;
+        //         }
+        //         if (event.detail.state) {
+        //           ambientLight["Sensor"] = event.detail.state;
+        //         }
+        //       }
+        //     break;
 
-            default:
-              break;
-          }
+        //     default:
+        //       break;
+        //   }
 
-          return ambientLight;
+        //   return ambientLight;
 
-        }
+        // }
 
       }
 
