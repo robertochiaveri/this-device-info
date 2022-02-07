e8db2b3329b727735cea58ef8b0c5b923b1c78bc
Roberto Chiaveri <robertochiaveri@users.noreply.github.com>
2022-02-07 01:00:40 +0100

    user input type detection

--- src/modules/uiModeInfo.js
+++ src/modules/uiModeInfo.js
@@ -2,35 +2,75 @@ module.exports = (function() {
 
   "use strict";
 
-  /* private vars and methods... */
-  var otherFunction = function () {
+  if (typeof pointerType == "undefined") {
+    var pointerType = "";
+  } 
+  var getPointerType = function(event) {
+
+    console.log("detecting pointerType...",event.type);
+
+    var onlyUnique = function(value, index, self) {
+      return self.indexOf(value) === index;
+    }
+
+    if (typeof pointerType == "string") {
+      if (pointerType.length == 0) {
+        pointerType = [];            
+      } else {
+        pointerType = pointerType.split(",");            
+      }
+    }
+
+    switch (event.pointerType) {
+      case 'mouse':
+        pointerType.push("mouse or touchpad");
+        break;
+      case 'pen':
+        pointerType.push("styus");
+        break;
+      case 'touch':
+        pointerType.push("touchscreen");
+        break;
+      default:
+        pointerType.push(event.pointerType);
+    }
+
+    pointerType = pointerType.filter(onlyUnique).join(", ");
+
+    console.log("pointerType is "+pointerType);  
+
+    return pointerType;
+
   }
-
-
+  
+  
   var init = function(event) {
 
     var uimode = {};
 
     if (typeof window.matchMedia == "function") {
         
-        if (window.matchMedia("screen and (prefers-color-scheme: dark)").matches) {
-            uimode.theme = "dark ";
-        }
+      if (window.matchMedia("screen and (prefers-color-scheme: dark)").matches) {
+          uimode.theme = "dark ";
+      }
 
-        if (window.matchMedia("screen and (prefers-color-scheme: light)").matches) {
-            uimode.theme = "light";
-        }  
-
-        return uimode;
-
+      if (window.matchMedia("screen and (prefers-color-scheme: light)").matches) {
+          uimode.theme = "light";
+      }
+      
     }
 
+    if (event && (event.type == "pointerdown" || event.type == "pointermove" || event.type == "scroll") && event.pointerType ) {
+      uimode.pointerType = getPointerType(event);
+    };  
+    
+  return uimode;
   }
 
   /* public methods... */
   return {
     init : init,
-    defaultListeners : ["DOMContentLoaded"]
+    defaultListeners : ["DOMContentLoaded","pointerdown","pointermove"]
   };
 })();
 
