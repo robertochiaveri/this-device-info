726700abf8c8815a18d063cc3d2a759424f6d96f
Roberto Chiaveri <robertochiaveri@users.noreply.github.com>
2022-02-07 19:14:44 +0100

    try a more accurate way to measure refresh rate

--- src/modules/screenRefreshRate.js
+++ src/modules/screenRefreshRate.js
@@ -23,7 +23,41 @@ module.exports = (function() {
     });
   };
   
-  getFPS();
+  /* getFPS(); */
+	
+'use strict';
+
+  var previousTimestamp, divInterval, divFPS;
+
+  var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+  var rafLoopCount = 0;
+  var rafLoopMax = 30;
+  var rafLoop = function rafLoop(timestamp) {
+      var interval = timestamp - previousTimestamp;
+      var fps = 1000 / interval;
+      previousTimestamp = timestamp;
+      if (rafLoopCount >= rafLoopMax) {
+        console.log("fps detection completed");
+        var screenFPSInfoEvent = new CustomEvent("__ScreenFPSInfoEvent", {
+          detail: {
+            fps: parseInt(fps)
+          },
+          bubbles: true,
+          cancelable: true
+        })
+        dispatchEvent(screenFPSInfoEvent);  
+      } else {
+        rafLoopCount++;
+        raf(rafLoop);
+      }
+  };
+
+  // This is run first to set the previousTimestamp variable with an initial value, and then call the rafLoop function.
+  raf(function (timestamp) {
+      previousTimestamp = timestamp;
+      raf(rafLoop);
+  });	
 
   var init = function(event) {
     if (event && event.type == "__ScreenFPSInfoEvent" && event.detail ) {
