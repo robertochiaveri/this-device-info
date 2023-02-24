ed78957e0d5d376db5d8e1a973f97bc96a91374d
Roberto Chiaveri <robertochiaveri@users.noreply.github.com>
2023-02-24 18:07:38 +0100

    Update ipLookup.php

--- dist/php/ipLookup.php
+++ dist/php/ipLookup.php
@@ -5,15 +5,35 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+/*
+if($_SERVER['HTTP_REFERER'] !== "SET_ALLOWED_DOMAIN_HERE"){
+    die('Unauthorized access');
+}
+*/
+
 // check for cURL
 if (!extension_loaded("curl")) {
     die("Error: cURL not loaded.");
 }
 
-// Specify the URL of the web server to make the request to
-$url = "SET_ENDPOINT_HERE";
 
-echo "url: ".$url;
+//whether ip is from the share internet  
+if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+    $ip = $_SERVER['HTTP_CLIENT_IP'];  
+}  
+//whether ip is from the proxy  
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+ }  
+//whether ip is from the remote address  
+else{  
+    $ip = $_SERVER['REMOTE_ADDR'];  
+}  
+// in case of multiple proxies, use the first IP https://en.wikipedia.org/wiki/X-Forwarded-For
+$ip = array_shift(explode(",", $ip ));
+
+// Specify the URL of the web server to make the request to
+$url = "SET_ENDPOINT_HERE"."/".$ip;
 
 // Initialize a new cURL session
 $curl = curl_init();
@@ -34,18 +54,14 @@ $response = curl_exec($curl);
 
 // Check if there was an error with the cURL request
 if(curl_errno($curl)) {
-    echo "Error: " . curl_error($curl);
-} else {
-    // Decode the JSON response
-    $data = json_decode($response);
+    die "Error: " . curl_error($curl);
+} 
+echo $response;
 
-    // Display the response
-    var_dump($data);
-}
 
 // Close the cURL session
 curl_close($curl);
 
-die("x_x");
+die();
 
 ?>
