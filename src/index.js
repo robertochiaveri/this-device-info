91815eb7cc3018926202095074e2263113c424f5
Roberto Chiaveri <robertochiaveri@users.noreply.github.com>
2024-09-27 15:55:28 +0200

    adding magnetic sensor to init flow

--- src/index.js
+++ src/index.js
@@ -67,6 +67,8 @@ thisDeviceInfo.loadModule("pushNotificationsInfo", require('./modules/pushNotifi
 
 thisDeviceInfo.loadModule("screenRefreshRateInfo", require('./modules/screenRefreshRate'));
 
+thisDeviceInfo.loadModule("magnetometerInfo", require('./modules/magnetometerInfo'));
+
 thisDeviceInfo.init({
   callbackFn: require("./template/default/js/output")
 });
